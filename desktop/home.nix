8c818edfbff37066773c8ad96293e6cdada02a4e
rsmith8917 <rsmith8917@gmail.com>
2024-11-01 09:11:07 -0400

    cleanup

--- desktop/home.nix
+++ desktop/home.nix
@@ -4,15 +4,7 @@
   home.username = "robert";
   home.homeDirectory = "/home/robert";
 
-  imports = [
-    ../shared/home/cli-utils.nix
-    ../shared/home/git.nix
-    ../shared/home/zellij.nix
-    ../shared/home/alacritty.nix
-    ../shared/home/gui-utils.nix
-    ../shared/home/vscode.nix
-    ../shared/home/gnome.nix
-  ];
+  imports = [ ../shared/home ];
 
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
