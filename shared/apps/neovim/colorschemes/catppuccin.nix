eea32b8c3b31c82d9cf666e4fc04d989539ad697
rsmith8917 <rsmith8917@gmail.com>
2024-11-27 13:41:05 -0500

    catppuccin flavours

--- shared/apps/neovim/colorschemes/catppuccin.nix
+++ shared/apps/neovim/colorschemes/catppuccin.nix
@@ -2,5 +2,35 @@
 {
   programs.nixvim.colorschemes.catppuccin = {
     enable = true;
+    settings = {
+      flavour = "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+      integrations = {
+        cmp = true;
+        notify = true;
+        neotree = true;
+        which_key = true;
+        treesitter = true;
+        telescope.enabled = true;
+        native_lsp = {
+          enabled = true;
+          inlay_hints = {
+            background = true;
+          };
+          virtual_text = {
+            errors = [ "italic" ];
+            hints = [ "italic" ];
+            information = [ "italic" ];
+            warnings = [ "italic" ];
+            ok = [ "italic" ];
+          };
+          underlines = {
+            errors = [ "underline" ];
+            hints = [ "underline" ];
+            information = [ "underline" ];
+            warnings = [ "underline" ];
+          };
+        };
+      };
+    };
   };
 }
